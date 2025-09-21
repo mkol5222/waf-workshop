fc3d1fbef93b8d9bb0a845f98cc579af746a6af5
Martin Koldovsky <mkoldov@gmail.com>
2025-09-21 13:41:57 +0000

    ssh vmss

--- azure-vmss/NOTES.md
+++ azure-vmss/NOTES.md
@@ -1,5 +1,28 @@
 
 
+# ssh
+
+```bash
+az vmss list -o table
+# waf-tf-vmss-6b4d6f7e 
+# rg WAF-WORKSHOP-VMSS-6B4D6F7E-RG
+ENVID=$(npx @dotenvx/dotenvx get TF_VAR_envId2)
+echo $ENVID
+RG=$"WAF-WORKSHOP-VMSS-$ENVID-RG"
+echo $RG
+
+VMSSNAME=$"waf-tf-vmss-$ENVID"
+echo $VMSSNAME
+
+az vmss show -g $RG --name $VMSSNAME
+
+# public IPs of VMSS
+az vmss list-instance-public-ips -g $RG --name $VMSSNAME -o table
+az vmss list-instance-public-ips --help 
+PUBPIPS=$(az vmss list-instance-public-ips -g $RG --name $VMSSNAME --query "[].{ip:ipAddress}" -o tsv)
+echo $PUBPIPS
+```
+
 # customizations
 
 ```bash
---
+++ azure-vmss/ssh.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "SSH into a VMSS instance"
+source ./tfvars.sh
+
+# az vmss list -o table
+
+# waf-tf-vmss-6b4d6f7e 
+# rg WAF-WORKSHOP-VMSS-6B4D6F7E-RG
+ENVID=$(npx @dotenvx/dotenvx get -f ../.env -fk ../.env.keys TF_VAR_envId2)
+
+echo  ENV1 $(npx @dotenvx/dotenvx get -f ../.env -fk ../.env.keys TF_VAR_envId2)
+
+echo ENV2 $ENVID
+RG="WAF-WORKSHOP-VMSS-$ENVID-RG"
+# echo $RG
+
+VMSSNAME="waf-tf-vmss-$ENVID"
+# echo $VMSSNAME
+
+echo "VMSS Name: $VMSSNAME in RG: $RG" 
+
+# instance # from script args 0,1,2...
+INSTANCEID=${1:-0}
+echo "Instance ID: $INSTANCEID"
+
+PUBLICIP=$(az vmss list-instance-public-ips --name $VMSSNAME --resource-group $RG --query "[$INSTANCEID].ipAddress" -o tsv)
+# if not found, exit
+if [ -z "$PUBLICIP" ]; then
+  echo "No public IP found for instance ID $INSTANCEID"
+  exit 1
+fi
+echo "Public IP: $PUBLICIP"
+
+SSHPASS=$(npx @dotenvx/dotenvx get -f ../.env -fk ../.env.keys TF_VAR_admin_password)
+
+echo "SSHing into VMSS instance at $PUBLICIP with password $SSHPASS"
+sshpass -p "$SSHPASS" ssh -o StrictHostKeyChecking=no admin@$PUBLICIP
